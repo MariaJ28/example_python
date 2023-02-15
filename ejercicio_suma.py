6b5a7960512efc7ea0b8879c7a2e120706318d82
MariaJ28 <mjortiz627@soy.sena.edu.co>
2023-02-15 11:20:31 -0500

    se creo comentarios y la operacion restar

--- ejercicio_suma.py
+++ ejercicio_suma.py
@@ -1,7 +1,22 @@
+# ejercicio sumar dos numeros
+
+#entrada
 num1 = 6
 num2 = 3
 
+#proceso
 suma = num1 + num2
 
+#salida
 print(suma)
 
+""" comentarios de varios renglones"""
+# comentarios en un solo renglon
+
+#ejercicio restar dos numeros
+
+#proceso
+resta= num1 - num2
+
+#salida
+print(resta)
