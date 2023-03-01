aa7f68879397eb7b89cfa73acfb482870bd58ce4
MariaJ28 <mjortiz627@soy.sena.edu.co>
2023-03-01 10:48:56 -0500

    caso_mercado

---
+++ mercado.py
@@ -0,0 +1,23 @@
+nombre_cliente=input("digita tu nombre completo: ")
+docuento=input("digita tu numero de documento:")
+
+productos={
+    "jabon liquido": 10000,
+    "sal": 5000,
+    "papel higienico": 20000,
+    "aceite":12000,
+    "mecato":25000
+}
+
+print (productos)
+precio=productos["aceite"]+ productos["jabon liquido"]+productos["mecato"]+productos["papel higienico"]+productos["sal"]
+print("total del mercado: ",precio)
+
+saldo=900000
+valor_domicilio=10000
+total_compra=precio + valor_domicilio
+
+if saldo >= total_compra:
+    print("tu compra sera entregada por un domiciliario con un total de: ",total_compra)
+else:
+    print("no te alcanza para llevarlo en domicilio ya que tu presupuesto es de: ",saldo)
